68595b54d113a92da6a0e430a907e5d73cecee47
IvanKholoshyn <ivan.kholoshyn2006@gmail.com>
2025-07-06 01:37:16 +0300

    Updated app template
    Changed navigation panel, created placeholders for the Main and Statistics tabs. Visual is WIP.

--- SwanApp/Swan/app/(tabs)/stats.tsx
+++ SwanApp/Swan/app/(tabs)/stats.tsx
@@ -1,9 +1,9 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function HomeScreen() {
+export default function StatsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text}>Statistics Tab</Text>
       <Text style={styles.text}>Swan</Text>
       <Button title="press me" onPress={() => alert("button pressed")} />
     </View>
@@ -13,7 +13,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center',
   },
